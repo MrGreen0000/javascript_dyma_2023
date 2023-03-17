8ef180cef624cfae01179ed7211f10444c78e8f9
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-03-17 17:25:23 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,35 +1,13 @@
-const condition = true;
+// for (let i = 0; i < 4; i++) {
+//   console.log(i);
+// }
+// for (let j = 0; j < 4; ) {
+//   console.log(j);
+//   i++;
+// }
 
-if (condition === true) {
-  console.log("ici");
-}
-
-const age = 18;
-switch (age) {
-  case 21:
-    console.log("Tu peux boire de l’alcool aux Etats-Unis.");
+for (let i = 0; i < t.length; i++) {
+  if (t[i] === 25) {
     break;
-  case 18:
-    console.log("Tu peux boire de l’alcool en France.");
-    break;
-  case 16:
-    console.log("Tu peux boire de l’alcool à Cuba.");
-    break;
-  case 14:
-    console.log("Tu peux boire de la bière en Allemagne.");
-    break;
-  default:
-    console.log("Rien pour toi cette année.");
-}
-const pays = "Maroc";
-switch (pays) {
-  case "France":
-  case "Allemagne":
-  case "Italie":
-  case "Espagne":
-    console.log("Tu es européen !");
-    break;
-  case "Corée":
-  default:
-    console.log("Tu n’es pas européen.");
+  }
 }
