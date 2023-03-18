4513f14d6a80fcceeab67198c064485c43c23146
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-03-18 08:33:18 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-// for (let i = 0; i < 4; i++) {
-//   console.log(i);
-// }
-// for (let j = 0; j < 4; ) {
-//   console.log(j);
-//   i++;
-// }
+// Number.isSafeInteger(9007199254740991); // true;
+// Number.isSafeInteger(9007199254740992); // fa
 
-for (let i = 0; i < t.length; i++) {
-  if (t[i] === 25) {
-    break;
-  }
-}
+console.log(Number.isSafeInteger(9007199254740992));
+
+console.log(0.1 + 0.2); // 0.30000000000000004
+console.log(0.1 * 0.2); // 0.020000000000000004
+console.log(0.11 / 0.1); // 1.0999999999999999
+console.log(123456789012345678); // 123456789012345680
+console.log(0.1 * 2);
