3d567025a2a8642d5ad12cc4f44f18424045d30b
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-03-18 09:04:02 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-// Number.isSafeInteger(9007199254740991); // true;
-// Number.isSafeInteger(9007199254740992); // fa
+console.log((0.2 * 100 + 0.1 * 100) / 100);
+console.log(1e6);
+console.log(2 ** 3);
 
-console.log(Number.isSafeInteger(9007199254740992));
+console.log(parseFloat(" 32.21frefz"));
+parseFloat("   0.152e57  "); // 1.52e+56
+parseFloat(".1"); // 0.1
+parseFloat("a1"); // NaN
 
-console.log(0.1 + 0.2); // 0.30000000000000004
-console.log(0.1 * 0.2); // 0.020000000000000004
-console.log(0.11 / 0.1); // 1.0999999999999999
-console.log(123456789012345678); // 123456789012345680
-console.log(0.1 * 2);
+console.log(parseInt(" 32.21frefz", 10));
+console.log(parseInt("10101", 2));
+
+console.log(+"78");
+console.log(3 + +"78");
+
+console.log(parseInt("-."));
+console.log(Number.isNaN(undefined));
