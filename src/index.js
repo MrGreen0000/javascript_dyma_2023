4fcdcc4a19c53ec22edfafd6ea427d0b170924f4
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-03-23 20:39:40 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
-const regex1 = /.+/;
-const regex2 = new RegExp("w+");
-const regex3 = RegExp("w+");
+const obj = { a: {}, b: 42, c: true, d: "test" };
 
-const regexp = RegExp("test.", "g");
-const chaine = "Une chaîne pour tester et effectuer des tests";
-const matches = chaine.matchAll(regexp);
+console.log(JSON.stringify(obj));
 
-for (const match of matches) {
-  console.log(match);
-}
+const json = JSON.stringify({ a: 1, b: 2 });
+const objet = JSON.parse(json);
 
-console.log(/a/.test("abc"));
-RegExp(12).test(8768912);
-
-RegExp("d").test("fezezf2");
-/\d/.test(4);
-/\d/.test("bonjour");
-
-/\D/.test("bonjour");
-/\D/.test(4);
-
-console.log("mon_super_fichier.pdf".match(/(^\w+)\.([a-z]{3}$)/));
-
-console.log(
-  /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).{10,}/.test("hE1!frezzfe")
-);
-
-console.log(/\d+\w\d+/.test("42_12"));
+console.log(objet);
