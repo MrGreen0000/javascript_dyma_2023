8fb4eedfacb1dc44833500b3cfa3f85336f3c91e
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-06-01 15:14:04 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
-// const tableau = ["a", "b", "c"];
-// console.log(tableau[tableau.length - 3]); // c
+import "./style.css";
 
-// const tableau = [1, 2, 42];
-// tableau.length = 0;
-// console.log(tableau);
-
-// tableau[0] = 1;
-// console.log(tableau);
-
-// const tableau = [10];
-// const [a = 0, b = 2] = tableau;
-// console.log(a);
-// console.log(b);
-
-const tableau = [1, 2, 3];
-tableau.unshift(4);
-const retour = tableau.unshift(5, 6);
-console.log(tableau); // [5, 6, 4, 1, 2, 3]
-console.log(retour); // 6
+const p = document.querySelector("p");
+const img = document.querySelector("img");
+const section = document.querySelector("section");
+const a = document.querySelector("a");
+const input = document.querySelector("input");
