629ec4f6308dc5aebb67f08bbe9ffe583c31114e
Naji Mouflih <nmouflih@bluesoft-group.com>
2023-09-17 18:06:06 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-const promesse = fetch("https://jsonplaceholder.typicode.com/users");
+const user = {
+  name: "tintin",
+  email: "tintin@gmail.com",
+};
+
+const promesse = fetch("https://jsonplaceholder.typicode.com/users", {
+  method: "POST",
+  body: JSON.stringify(user),
+  headers: {
+    "Content-type": "application/json",
+  },
+});
 
 promesse
   .then(async (response) => {
-    console.log(response);
     try {
-      const users = await response.text();
-      console.log(JSON.parse(users));
+      console.log(response);
+      const body = await response.json();
+      console.log(body);
     } catch (error) {
       console.log(error);
     }
